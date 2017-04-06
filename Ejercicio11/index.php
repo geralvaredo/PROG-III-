a943bdac7a0d78c28bcbb36e150cb198e6f58082
germangit <geralvaredo@gmail.com>
2017-04-06 17:58:35 -0300

    potencia

---
+++ Ejercicio11/index.php
@@ -0,0 +1,16 @@
+<?php 
+
+	
+
+
+	$objeto1 = pow(1,2) ;
+	$objeto2 = pow(2,2) ;
+	$objeto3 = pow(3,2) ;
+	$objeto4 = pow(4,2) ;
+
+	echo "potencia de 1:" . $objeto1 . "<br>" ;
+	echo "potencia de 2:" . $objeto2 . "<br>";
+	echo "potencia de 3:" . $objeto3 . "<br>";
+	echo "potencia de 4:" . $objeto4 . "<br>" ;
+
+ ?>
