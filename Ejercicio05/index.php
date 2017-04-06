716811f1a74817ca339e46ebc8362044f4ff8adb
germangit <geralvaredo@gmail.com>
2017-04-06 17:49:35 -0300

    aparecer nombre del numero de la variable

---
+++ Ejercicio05/index.php
@@ -0,0 +1,147 @@
+<?php
+
+$num = rand(20,60) ;
+echo $num  . "<br>";
+function veinti ($num)
+{
+    switch ($num) 
+    {
+    case '20':
+        return  "veinte" ;
+        break;
+    case '21':
+        return  "veintiuno" ;
+        break;
+   case '22':
+     return   "veintidos" ;
+        break;
+   case '23':
+      return  "veintitres" ;
+        break;
+   case '24':
+     return     "veinticuatro" ;
+        break;
+        case '25':
+    return      "veinticinco" ;
+        break;
+        case '26':
+      return "veintiseis" ;
+        break;
+        case '27':
+     return    "veintisiete" ;
+        break;
+        case '28':
+       return   "veintiocho" ;
+        break;
+        case '29':
+       return   "veintinueve" ;
+        break;
+         case '30':
+       return   "treinta" ;
+        break;
+         case '31':
+       return   "treinta y uno" ;
+        break;
+         case '32':
+       return   "treinta y dos" ;
+        break;
+         case '33':
+       return   "treinta y tres " ;
+        break;
+         case '34':
+       return   "treinta y cuatro" ;
+        break;
+         case '35':
+       return   "treinta y cinco" ;
+        break;
+         case '36':
+       return   "treinta y seis" ;
+        break;
+         case '37':
+       return   "treinta y sieta" ;
+        break;
+         case '38':
+       return   "treinta y ocho" ;
+        break;
+         case '39':
+       return   "treinta y nueve" ;
+        break;
+         case '40':
+       return   " cuarenta" ;
+        break;
+         case '41':
+       return   "cuarenta y uno " ;
+        break;
+         case '42':
+       return   "cuarenta y dos" ;
+        break;
+         case '43':
+       return   "cuarenta y tres " ;
+        break;
+         case '44':
+       return   "cuarenta y cuatro" ;
+        break;
+         case '45':
+       return   "cuarenta y cinco" ;
+        break;
+         case '46':
+       return   "cuarenta y seis" ;
+        break;
+         case '47':
+       return   "cuarenta y siete " ;
+        break;
+         case '48':
+       return   "cuarenta y ocho" ;
+        break;
+         case '49':
+       return   " cuarenta y nueve" ;
+        break;
+         case '50':
+       return   "cincuenta" ;
+        break;
+         case '51':
+       return   "cincuenta y uno" ;
+        break;
+         case '52':
+       return   "cincuenta y dos " ;
+        break;
+         case '53':
+       return   "cincuenta y tres" ;
+        break;
+         case '54':
+       return   "cincuenta y cuatro" ;
+        break;
+         case '55':
+       return   "cincuenta y cinco" ;
+        break;
+         case '56':
+       return   "cincuenta y seis" ;
+        break;
+         case '57':
+       return   "cincuenta y siete" ;
+        break;
+         case '58':
+       return   "cincuenta y ocho " ;
+        break;
+         case '59':
+       return   "cincuenta y nueve" ;
+        break;
+         case '60':
+       return   "sesenta " ;
+        break;
+         
+
+
+}
+
+
+}
+
+$palabra = veinti($num) ;
+
+echo $palabra ;
+
+
+
+
+ ?>
