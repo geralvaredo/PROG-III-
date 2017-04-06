f4dddb1c5dbd0962e49997ee5fe7aee7d6387fa0
germangit <geralvaredo@gmail.com>
2017-04-06 18:00:29 -0300

    funcion par e impar

---
+++ Ejercicio14/index.php
@@ -0,0 +1,23 @@
+<?php 
+
+function esPar($num)
+{
+    if($num%2==0)
+    
+    return true ;
+    
+    else 
+    
+    return false;
+    
+}
+
+function esImpar()
+{
+   if(!esPar($num))
+   return true;
+   else
+   return false ; 
+}
+
+?>
